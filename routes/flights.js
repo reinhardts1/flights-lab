5ece4c5496b902f23a6cc76b27121b4626053c64
Rinhard javed <rinhardjaved@gmail.com>
2022-09-29 09:03:12 -0400

    meal fix

--- routes/flights.js
+++ routes/flights.js
@@ -7,17 +7,34 @@ import * as flightsCtrl from '../controllers/flights.js'
 
 
 
+
 router.get('/', flightsCtrl.index)
+
 router.get('/new', flightsCtrl.new)
 
+
 router.get('/:id', flightsCtrl.show)
+
+
 router.get("/:id/edit", flightsCtrl.edit)
+
 router.post('/', flightsCtrl.create)
+
+
 router.post('/:id/tickets', flightsCtrl.createTicket)
+
+
 router.post('/:id/meals', flightsCtrl.newMeal);
+
+
 router.delete('/:id', flightsCtrl.delete)
+
+
 router.delete('/:id/tickets/:ticketId', flightsCtrl.deleteTicket)
+
+
 router.put("/:id", flightsCtrl.update)
+
 
 export {
   router
